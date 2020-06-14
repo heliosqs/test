d7c967ca36e8eff4bf60cc65b9e23e4ca90f27a8
Elio Quevedo <heliosqs>
2020-06-14 17:16:14 -0500

    Corrección de dependencias en el Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,12 @@
 FROM node:12.13.1-alpine AS build
-WORKDIR /build
-COPY ./src/ /build/
-RUN npm install --production \
-    npm install -g babel-cli \
-    npm run build
-
-FROM node:12.13.1-alpine
 RUN apk add tzdata
 ENV TZ "America/Guayaquil"
-RUN mkdir /server
 RUN addgroup -S -g 1680 developers 2> /dev/null && \
     adduser -S -u 1680 -D -H -h /usr/lib/developers -s /sbin/nologin -G developers -g developers developers 2>/dev/null
-COPY --from=build /build/dist/ /server/dist
-COPY --from=build /build/config/ /server/config
-COPY --from=build /build/node_modules/ /server/node_modules
-COPY --from=build /build/package.json /server/
-WORKDIR "/server"
+WORKDIR /build
+COPY ./src/ /build/
+RUN npm install
+RUN npm run build
 USER developers
 EXPOSE 8000
 CMD npm run start
--- src/server/api/developer/serialize.js
+++ src/server/api/developer/serialize.js
@@ -1,41 +1,41 @@
 // A function that converts the body of a developer API request into the internal domain representation
 const _singleInstanceSerializer = (developer) => {
-    const newDeveloper = {
-        id: developer.id,
-        completeName: developer.nombres_completos,
-        knownTechnologies: developer.tecnologias_conocidas,
-        githubUrl: developer.link_github
-    }
-    return newDeveloper
+  const newDeveloper = {
+    id: developer.id,
+    completeName: developer.nombres_completos,
+    knownTechnologies: developer.tecnologias_conocidas,
+    githubUrl: developer.link_github
+  }
+  return newDeveloper
 }
 
 const serializer = (data) => {
-    if (!data) {
-        return null
-    }
-    if (Array.isArray(data)) {
-        return data.map(_singleInstanceSerializer)
-    }
-    return _singleInstanceSerializer(data)
+  if (!data) {
+    return null
+  }
+  if (Array.isArray(data)) {
+    return data.map(_singleInstanceSerializer)
+  }
+  return _singleInstanceSerializer(data)
 }
 
 const _singleInstanceDeserializer = (developer) => {
-    return {
-        id: developer.id,
-        nombres_completos: developer.completeName,
-        tecnologias_conocidas: developer.knownTechnologies,
-        link_github: developer.githubUrl
-    }
+  return {
+    id: developer.id,
+    nombres_completos: developer.completeName,
+    tecnologias_conocidas: developer.knownTechnologies,
+    link_github: developer.githubUrl
+  }
 }
 
 const deserializer = (data) => {
-    if (!data) {
-        return null
-    }
-    if (Array.isArray(data)) {
-        return data.map(_singleInstanceDeserializer)
-    }
-    return _singleInstanceDeserializer(data)
+  if (!data) {
+    return null
+  }
+  if (Array.isArray(data)) {
+    return data.map(_singleInstanceDeserializer)
+  }
+  return _singleInstanceDeserializer(data)
 }
 
-module.exports = {serializer, deserializer}
+module.exports = { serializer, deserializer }
